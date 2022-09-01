2a145e130db8db1176ba9cb94426189e9b575adc
Tanner Gladson <tannergladson@gmail.com>
2022-09-01 11:00:38 -0400

    first commit

---
+++ firstFile.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(void) {
+	
+    float firstFloat = 0;
+	float secondFloat = 0;
+	
+    printf("enter your first float: ");
+    int numInputs = scanf("%f %f", &firstFloat, &secondFloat);
+	
+	
+	printf("%.2f / %.2f = %.2f\n", firstFloat, secondFloat, firstFloat/secondFloat);
+	printf("%.2f + %.2f = %.2f\n", firstFloat, secondFloat, firstFloat+secondFloat);
+	printf("%.2f - %.2f = %.2f\n", firstFloat, secondFloat, firstFloat-secondFloat);
+	printf("%.2f * %.2f = %.2f\n", firstFloat, secondFloat, firstFloat*secondFloat);	
+    
+    return 0;
+	
+}
